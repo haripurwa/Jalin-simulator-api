60dd47a139c4a55944915e35e807cf019459cd49
HariPurwanto <haripurwanto446@gmail.com>
2023-07-27 07:26:04 +0700

    first commit

---
+++ src/main/java/jpn/jalin/JalinApplication.java
@@ -0,0 +1,14 @@
+package jpn.jalin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JalinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JalinApplication.class, args);
+		System.out.println("JALIN APPLICATION RUNNING............");
+	}
+
+}
---
+++ src/test/java/jpn/jalin/JalinApplicationTests.java
@@ -0,0 +1,13 @@
+package jpn.jalin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JalinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
